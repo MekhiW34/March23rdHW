4f0064b0b330c2a25b3b5afba748576728bf1fea
MekhiW34 <washington.mekhi@yahoo.com>
2021-03-23 11:37:10 -0400

    first commit

---
+++ guessgame/Launcher.java
@@ -0,0 +1,9 @@
+package guessgame;
+
+public class Launcher {
+
+    public static void main(String[] args)  {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
---
+++ videohw/GetMonth.java
@@ -0,0 +1,8 @@
+package videohw;
+
+public class GetMonth {
+    public static void main(String[] args) {
+        System.out.println(CodeAlong.getMonth(2));
+        System.out.println(CodeAlong.getMonth("January"));
+    }
+}
